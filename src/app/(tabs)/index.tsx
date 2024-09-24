602cd4767181f99fc2eaa4e2075e5d708f2ed09c
Otávio Araújo <otavio_araujo@hotmail.com>
2024-09-24 16:32:23 -0300

    1. Building UI

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,19 +1,19 @@
-import { StyleSheet } from "react-native";
+import { Image, StyleSheet } from "react-native";
 
 import { Text, View } from "@/src/components/Themed";
+import products from "@/assets/data/products";
+
+const product = products[0];
 
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Pizza Peperoni</Text>
+    <View className="p-4 bg-white rounded-2xl">
+      <Image
+        source={{ uri: product.image }}
+        className="w-[100%] aspect-square"
+      />
+      <Text className="text-lg font-semibold">{product.name}</Text>
+      <Text className="mt-2 font-bold text-blue-400">${product.price}</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
