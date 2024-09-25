d89e80a24708b7a783d30899e8d580be8fe1f203
Otávio Araújo <otavio_araujo@hotmail.com>
2024-09-25 00:03:46 -0300

    product-details-ui-done

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,14 +1,39 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import React from "react";
 import { Stack, useLocalSearchParams } from "expo-router";
+import products from "@/assets/data/products";
+import { defaultPizzaImage } from "@/src/components/Product/ProductListItem";
+
+const sizes = ["S", "M", "L", "XL"];
 
 const ProductDetailsScreen = () => {
   const { id } = useLocalSearchParams();
 
+  const product = products.find((p) => p.id.toString() === id);
+  if (!product) {
+    return <Text>Product not found</Text>;
+  }
+
   return (
-    <View>
-      <Stack.Screen options={{ title: `Product Details: ${id}` }} />
-      <Text>ProductDetailsScreen for id: {id}</Text>
+    <View className="flex-1 p-3">
+      <Stack.Screen options={{ title: product?.name }} />
+      <Image
+        source={{ uri: product.image || defaultPizzaImage }}
+        className="w-full aspect-square"
+      />
+
+      <Text className="text-sm font-bold">Select size:</Text>
+      <View className="flex-row justify-around my-3">
+        {sizes.map((size) => (
+          <View
+            key={size}
+            className="items-center justify-center w-12 h-12 bg-gray-300 rounded-full"
+          >
+            <Text className="text-lg font-medium">{size}</Text>
+          </View>
+        ))}
+      </View>
+      <Text className="text-lg font-bold">Price: ${product.price}</Text>
     </View>
   );
 };
