38b514d53d83f52fb0a4cbfdba80fa9852a40cb5
Otávio Araújo <otavio_araujo@hotmail.com>
2024-09-25 16:19:38 -0300

    shopping-cart-modal-created

--- src/app/(tabs)/menu/_layout.tsx
+++ src/app/(tabs)/menu/_layout.tsx
@@ -1,8 +1,27 @@
-import { Stack } from "expo-router";
+import { FontAwesome } from "@expo/vector-icons";
+import { Link, Stack } from "expo-router";
+import { Pressable } from "react-native";
 
 export default function MenuStack() {
   return (
-    <Stack>
+    <Stack
+      screenOptions={{
+        headerRight: () => (
+          <Link href="/cart" asChild>
+            <Pressable className="mt-1">
+              {({ pressed }) => (
+                <FontAwesome
+                  name="shopping-cart"
+                  size={20}
+                  color="#3b82f6"
+                  style={{ opacity: pressed ? 0.5 : 1 }}
+                />
+              )}
+            </Pressable>
+          </Link>
+        ),
+      }}
+    >
       <Stack.Screen name="index" options={{ title: "Menu" }} />
     </Stack>
   );
