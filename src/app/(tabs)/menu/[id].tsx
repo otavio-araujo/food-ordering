2fb99d04d5f51ff197e86b1c70a8612687eaea4d
Otávio Araújo <otavio_araujo@hotmail.com>
2024-09-24 23:32:58 -0300

    expo-router-screens

---
+++ src/app/(tabs)/menu/[id].tsx
@@ -0,0 +1,16 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { Stack, useLocalSearchParams } from "expo-router";
+
+const ProductDetailsScreen = () => {
+  const { id } = useLocalSearchParams();
+
+  return (
+    <View>
+      <Stack.Screen options={{ title: `Product Details: ${id}` }} />
+      <Text>ProductDetailsScreen for id: {id}</Text>
+    </View>
+  );
+};
+
+export default ProductDetailsScreen;
--- src/components/Product/ProductListItem.tsx
+++ src/components/Product/ProductListItem.tsx
@@ -1,6 +1,7 @@
-import { Image, StyleSheet } from "react-native";
+import { Image, Pressable } from "react-native";
 import { Text, View } from "@/src/components/Themed";
 import { Product } from "@/src/types";
+import { Link } from "expo-router";
 
 export const defaultPizzaImage =
   "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png";
@@ -11,17 +12,19 @@ type ProductListItemProps = {
 
 const ProductListItem = ({ product }: ProductListItemProps) => {
   return (
-    <View className="flex-1 p-2 max-w-[50%] bg-white rounded-2xl">
-      <View className="max-w-[80%] mx-auto">
-        <Image
-          source={{ uri: product.image || defaultPizzaImage }}
-          className="flex-1 aspect-square"
-          resizeMode="contain"
-        />
-      </View>
-      <Text className="text-sm font-semibold">{product.name}</Text>
-      <Text className="mt-1 font-bold text-blue-400">${product.price}</Text>
-    </View>
+    <Link href={`/menu/${product.id}`} asChild>
+      <Pressable className="flex-1 p-2 max-w-[50%] bg-white rounded-2xl">
+        <View className="max-w-[80%] mx-auto">
+          <Image
+            source={{ uri: product.image || defaultPizzaImage }}
+            className="flex-1 aspect-square"
+            resizeMode="contain"
+          />
+        </View>
+        <Text className="text-sm font-semibold">{product.name}</Text>
+        <Text className="mt-1 font-bold text-blue-400">${product.price}</Text>
+      </Pressable>
+    </Link>
   );
 };
 
