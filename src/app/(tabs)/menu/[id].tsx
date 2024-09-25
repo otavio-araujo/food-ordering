5b798eb52c5de311a34dead0a60e3efb2a356a82
Otávio Araújo <otavio_araujo@hotmail.com>
2024-09-25 17:20:29 -0300

    shopping-cart-code-half

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -4,20 +4,27 @@ import { Stack, useLocalSearchParams } from "expo-router";
 import products from "@/assets/data/products";
 import { defaultPizzaImage } from "@/src/components/Product/ProductListItem";
 import Button from "@/src/components/Forms/Button";
+import { useCart } from "@/src/providers/CartProvider";
+import { PizzaSize } from "@/src/types";
 
-const sizes = ["S", "M", "L", "XL"];
+const sizes: PizzaSize[] = ["S", "M", "L", "XL"];
 
 const ProductDetailsScreen = () => {
+  const { addItem } = useCart();
   const { id } = useLocalSearchParams();
-  const [selectedSize, setSelectedSize] = React.useState("M");
+  const [selectedSize, setSelectedSize] = React.useState<PizzaSize>("M");
   const product = products.find((p) => p.id.toString() === id);
+
+  const addToCart = () => {
+    if (!product) {
+      return;
+    }
+    addItem(product, selectedSize);
+  };
+
   if (!product) {
     return <Text>Product not found</Text>;
   }
-
-  const addToCart = () => {
-    Alert.alert("Add to Cart and Selected Size: " + selectedSize);
-  };
 
   return (
     <View className="flex-1 p-3">
