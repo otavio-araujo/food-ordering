4b5d6bfaa1ca60f93c8b15c0e9d403e9f6956787
Otávio Araújo <otavio_araujo@hotmail.com>
2024-09-27 22:27:01 -0300

    order-screens-users-side-done

--- src/components/Order/OrderItemListItem.tsx
+++ src/components/Order/OrderItemListItem.tsx
@@ -1,5 +1,6 @@
 import { OrderItem } from "@/src/types";
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
+import { defaultPizzaImage } from "../Product/ProductListItem";
 
 type OrderItemListItemProps = {
   item: OrderItem;
@@ -7,8 +8,26 @@ type OrderItemListItemProps = {
 
 const OrderItemListItem = ({ item }: OrderItemListItemProps) => {
   return (
-    <View className="flex-row items-center justify-between p-3 bg-white rounded-lg">
-      <Text>Here comes the items</Text>
+    <View className="flex-row items-center p-3 bg-white rounded-lg">
+      <Image
+        source={{ uri: item.products.image || defaultPizzaImage }}
+        className="w-20 aspect-square"
+        resizeMode="contain"
+      />
+      <View className="ml-3 gap-y-1">
+        <Text className="text-[16px] font-bold text-gray-800">
+          {item.products.name}
+        </Text>
+        <View className="flex-row gap-x-2">
+          <Text className="font-bold text-blue-500">
+            ${item.products.price}
+          </Text>
+          <Text className="font-medium text-gray-800">Size: {item.size}</Text>
+        </View>
+      </View>
+      <Text className="ml-auto text-xl font-bold text-gray-800">
+        {item.quantity}
+      </Text>
     </View>
   );
 };
