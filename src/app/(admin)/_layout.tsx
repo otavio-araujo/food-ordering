985e932826e35475903fe38bebb6a78e530f3b5e
Otávio Araújo <otavio_araujo@hotmail.com>
2024-09-27 22:29:34 -0300

    orders-screen-admin-done

--- src/app/(admin)/_layout.tsx
+++ src/app/(admin)/_layout.tsx
@@ -44,10 +44,10 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="two"
+        name="orders"
         options={{
           title: "Orders",
-          headerTitleAlign: "center",
+          headerShown: false,
           tabBarIcon: ({ color }) => <TabBarIcon name="list" color={color} />,
         }}
       />
